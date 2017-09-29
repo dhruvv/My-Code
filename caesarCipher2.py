b0a77022d2a994f9db0698d5067c6cd1a4dd84e2
Gautam Dayal <gautam.dayal@yahoo.com>
2017-09-29 20:36:57 +0530

    Update caesarCipher2.py

--- caesarCipher2.py
+++ caesarCipher2.py
@@ -20,7 +20,7 @@ def cipher():
                     count += 1
                 else:
                     break
-        cipherlist.append(alphabet[count + shift])
+            cipherlist.append(alphabet[count + shift])
     ciphertext = ''.join(cipherlist)
 
     answindow = Tk()
@@ -37,15 +37,15 @@ framet.pack(side = TOP)
 frameb = Frame(window, height = 100)
 frameb.pack(side = BOTTOM)
 
-heading = Label(window, text = 'Caesar Cipher', fg = "black", font = ("Courier", 36))
+heading = Label(window, text = 'Caesar Cipher', fg = "black", font = ("Courier", 36), bg = "gray")
 heading.pack(side = TOP)
 
-textlabel = Label(window, text = 'Text:     ', fg = "black", font = ("Courier", 14))
+textlabel = Label(window, text = 'Text:     ', fg = "black", font = ("Courier", 14), bg = "gray")
 textlabel.pack(side = LEFT)
 textentry = Entry(window, bd = 4,bg = "green")
 textentry.pack(side = LEFT)
 
-shiftlabel = Label(window, text = '     Shift Number:     ', fg = "black", font = ("Courier", 14))
+shiftlabel = Label(window, text = '     Shift Number:     ', fg = "black", font = ("Courier", 14), bg = "gray")
 shiftlabel.pack(side = LEFT)
 shiftentry = Entry(window, bd = 4, bg = "green")
 shiftentry.pack(side = LEFT)
